7ee1b0c4c48cdaec853c350117f76430becad741
Lester Casabuena <lcasabuena1994@gmail.com>
2021-03-28 11:36:00 +0000

    Final Version

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function compute()
+//Compute the Interest Value
+function compute() 
 {
     
    var principal = document.getElementById("principal").value;
@@ -10,24 +11,26 @@ function compute()
    var future = current_year + parseInt(years);
 
    document.getElementById('result').innerHTML = 
-   `If you deposit ${principal}, <br> 
-   at an interest rate of ${rate}%. <br>
-   You will receive an amount of ${interest}, <br>
-   in the year ${future}`
+   `If you deposit <span class="highlight">${principal}</span>, <br> 
+   at an interest rate of <span class="highlight">${rate}%</span>. <br>
+   You will receive an amount of <span class="highlight">${interest}</span>, <br>
+   in the year <span class="highlight">${future}</span>`
 }
 
+//Updates value of slider
 function sliderUpdate()
 {
     var rate =  document.getElementById("rate").value;
     document.getElementById('slider_value').innerHTML = `${rate}%`;
 }
 
+//Performs validation
 function validate()
 {
 const principal_input = document.getElementById("principal");
 var principal = principal_input.value;
 if (Number(principal) <= 0 || principal == ""){
-    alert("Enter a positive number for amount");
+    alert("Enter a positive number");
     principal_input.focus();
     return false;
 }
